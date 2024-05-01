fefb46582b345b3a0427297ba0077cb62358fee8
AlbertoLuque <157401388+albertoluques@users.noreply.github.com>
2024-04-30 20:54:50 -0700

    Add FileWriter

--- src/com/currencyexchange/exchangeconversor/modelos/JsonArchive.java
+++ src/com/currencyexchange/exchangeconversor/modelos/JsonArchive.java
@@ -1,4 +1,18 @@
 package com.currencyexchange.exchangeconversor.modelos;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class JsonArchive {
+    public void saveJson(String currencyDataAPI) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        FileWriter escritura = new FileWriter(("Historial.json"));
+        escritura.write(gson.toJson(currencyDataAPI));
+        escritura.close();
+    }
+
+
 }
