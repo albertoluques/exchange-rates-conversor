c6733983142ea5871aa1b91fa3df8a64433f1fe1
AlbertoLuque <157401388+albertoluques@users.noreply.github.com>
2024-05-01 10:22:38 -0700

    Rewrite strings in english language

--- src/com/currencyexchange/exchangeconversor/Principal.java
+++ src/com/currencyexchange/exchangeconversor/Principal.java
@@ -28,43 +28,42 @@ public class Principal {
 
         while (true) {
             String messaje = """
-                        \n ___________________________________________
-                        Seleccione una divisa del siguiente listado
-                        1. ARS - Peso argentino
-                        2. BOB - Bolivares 
-                        3. BRL - Real brasileño
-                        4. CLP - Peso chileno
-                        5. COP - Peso colombiano
-                        6. USD - Dolar estadounidense
-                        7. MXN - Peso mexicano
-                        9. Salir; 
-                        ___________________________________________\n
-                        """;
+                        \n*************************************************
+                        Select the currency number of the following list:
+                        1. ARS - Argentine Peso
+                        2. BOB - Bolivian Boliviano
+                        3. BRL - Brazilian Real
+                        4. CLP - Chilean Peso
+                        5. COP - Colombian Peso
+                        6. USD - United States Dollar
+                        7. MXN - Mexican Peso
+                        9. Exit
+                        *************************************************""";
             try {
 
                 // seleccion de divisa base
                 System.out.println(messaje);
-                System.out.println("Eliga la divisa de la que partira ");
+                System.out.println("Select the base currency: ");
                 int baseCode = Integer.valueOf(scan.nextLine());
 
                 if (baseCode == 9) {
-                    System.out.println("Saliendo del programa, gracias por utilizar!");
+                    System.out.println("Closing the program, thanks for using!");
                     break;
                 } else if (baseCode <= 0 || baseCode >= 10) {
-                    System.out.println("Opcion no disponible, reiniciando programa");
+                    System.out.println("Option not available, restarting the program");
                     continue;
                 }
 
                 // seleccion de divisa de conversion
                 System.out.println(messaje);
-                System.out.println("Elija el numero de divisa desea convertir:");
+                System.out.println("Select your target currency: ");
                 int targetCode = Integer.valueOf(scan.nextLine());
 
                 if (targetCode == 9) {
-                    System.out.println("Saliendo del programa, gracias por utilizar!");
+                    System.out.println("Closing the program, thanks for using!");
                     break;
                 }  else if (targetCode <= 0 || targetCode >= 10) {
-                    System.out.println("Opcion no disponible, reiniciando programa");
+                    System.out.println("Option not available, restarting the program");
                     continue;
                 }
 
@@ -79,18 +78,18 @@ public class Principal {
                 ConversionAString valor2 = new ConversionAString();
                 valor2.ConversionIntAString(targetCode);
 
-                System.out.println("Escriba la cantidad que desea convertir: ");
+                System.out.println("Write the value you want to convert: ");
                 double value1 = Double.parseDouble(scan.nextLine());
 
                 CurrencyDataAPI currency = conversionRate.currencySolicitation(valor1.divisaBase(), valor2.divisaTarget());
 
-                String mensaje = "El valor %.2f [%s] equivale a: %.2f [%s]".formatted(value1, valor1.divisaBase(), (value1 * currency.conversion_rate()), valor2.divisaTarget());
+                String mensaje = "The value of %.2f [%s] equals to: %.2f [%s]".formatted(value1, valor1.divisaBase(), (value1 * currency.conversion_rate()), valor2.divisaTarget());
                 System.out.printf(mensaje);
 
                 JsonArchive json = new JsonArchive();
                 json.saveJson(mensaje);
             } catch (Exception e) {
-                throw new NumberFormatException("El programa solo acepta numeros");
+                throw new NumberFormatException("The program only accepts numbers");
             }
         }
     }
