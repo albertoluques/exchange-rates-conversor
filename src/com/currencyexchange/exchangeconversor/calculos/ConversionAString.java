f723a1bc6de39e3b39803ce0849cc99a10bf53d7
AlbertoLuque <157401388+albertoluques@users.noreply.github.com>
2024-04-30 20:49:24 -0700

    Add switch statements to convert int to String

--- src/com/currencyexchange/exchangeconversor/calculos/ConversionAString.java
+++ src/com/currencyexchange/exchangeconversor/calculos/ConversionAString.java
@@ -1,4 +1,64 @@
 package com.currencyexchange.exchangeconversor.calculos;
 
 public class ConversionAString {
+
+    private String divisaBase;
+    private String divisaTarget;
+
+    public void ConversionIntAString(int i) {
+        switch (i) {
+            case 1:
+            this.divisaBase = "ARG";
+            break;
+            case 2:
+                this.divisaBase = "BOB";
+                break;
+            case 3:
+                this.divisaBase = "BRL";
+                break;
+            case 4:
+                this.divisaBase = "CLP";
+                break;
+            case 5:
+                this.divisaBase = "COP";
+                break;
+            case 6:
+                this.divisaBase = "USD";
+                break;
+            case 7:
+                this.divisaBase = "MXN";
+                break;
+        }
+        switch (i) {
+            case 1:
+                this.divisaTarget = "ARG";
+                break;
+            case 2:
+                this.divisaTarget = "BOB";
+                break;
+            case 3:
+                this.divisaTarget = "BRL";
+                break;
+            case 4:
+                this.divisaTarget = "CLP";
+                break;
+            case 5:
+                this.divisaTarget = "COP";
+                break;
+            case 6:
+                this.divisaTarget = "USD";
+                break;
+            case 7:
+                this.divisaTarget = "MXN";
+                break;
+        }
+    }
+
+    public String divisaBase() {
+        return this.divisaBase;
+    }
+    public String divisaTarget() {
+        return  this.divisaTarget;
+    }
+
 }
